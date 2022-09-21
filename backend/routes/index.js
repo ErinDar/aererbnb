c7095fe5d0af3a36c7a6cb98a3ef95d4883492fa
Erin Dar <erindar@Erins-iMac.local>
2022-09-21 16:29:12 -0400

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { json } = require('sequelize');
 const router = express.Router();
+const apiRouter = require('./api');
 
+
+router.use('/api', apiRouter);
 // router.get('/hello/world', function (req, res) {
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
 //     res.send('Hello World!');
@@ -16,3 +19,18 @@ router.get('/api/csrf/restore', (req, res) => {
 })
 
 module.exports = router;
+
+
+// for testing /api route
+// fetch('/api/test', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'XSRF-TOKEN': 'f1rBSyVx-1-tDeXaD58DRG-zajFeUwQrSRvo'
+//     },
+//     body: JSON.stringify({
+//         hello: 'world'
+//     })
+// })
+//     .then(res => res.json())
+//     .then(data => console.log(data));
