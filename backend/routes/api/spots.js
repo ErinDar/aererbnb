0621f146b2ff430dda0d76bc8d312977cca12e7e
Erin Dar <erindar@Erins-iMac.local>
2022-09-30 13:22:46 -0400

    Updated get all spots route to translate to postgresSQL properly

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -79,14 +79,13 @@ router.get('/', async (req, res, next) => {
                 [
                     sequelize.literal(`(
                         SELECT AVG("Reviews".stars) FROM "Reviews"
-                        WHERE "Reviews".spotId = "Spot".id
+                        WHERE "Reviews".spotId = "Spots".id
                     )`), 'avgRating'
                 ],
                 [
                     sequelize.literal(`(
                         SELECT url FROM "SpotImages"
-                        WHERE "SpotImages".spotId = "Spot".id
-                        WHERE preview = true
+                        WHERE "SpotImages".spotId = "Spots".id, preview = true
                     )`), 'previewImage'
                 ]
             ]
@@ -109,13 +108,13 @@ router.get('/current', requireAuth, async (req, res, next) => {
                     [
                         sequelize.literal(`(
                         SELECT AVG("Reviews".stars) FROM "Reviews"
-                        WHERE "Reviews".spotId = "Spot".id
+                        WHERE "Reviews".spotId = "Spots".id
                     )`), 'avgRating'
                     ],
                     [
                         sequelize.literal(`(
                         SELECT url FROM "SpotImages"
-                        WHERE "SpotImages".spotId = "Spot".id
+                        WHERE "SpotImages".spotId = "Spots".id
                         WHERE preview = true
                     )`), 'previewImage'
                     ]
