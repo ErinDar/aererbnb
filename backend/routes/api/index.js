c8e5eebd15829e6d152b44d1b85d53645c58bfb7
Erin Dar <erindar@Erins-iMac.local>
2022-10-02 18:13:08 -0400

    Updated routes index and api index cleaning up unused code

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { restoreUser } = require('../../utils/auth');
 const sessionRouter = require('./session');
 const usersRouter = require('./users');
 const spotsRouter = require('./spots');
@@ -17,29 +17,5 @@ router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
 router.use('/spot-images', spotImgRouter);
 router.use('/review-images', reviewImgRouter);
-
-router.post('/test', (req, res) => {
-    res.json({
-        requestBody: req.body
-    })
-})
-
-// router.get('/set-token-cookie', async (req, res) => {
-//     const user = await User.findOne({
-//         where: {
-//             username: 'Demo-lition'
-//         }
-//     })
-//     setTokenCookie(res, user)
-//     return res.json({ user })
-// })
-
-// router.get('/restore-user', (req, res) => {
-//     return res.json(req.user)
-// })
-
-// router.get('/require-auth', requireAuth, (req, res) => {
-//     return res.json(req.user)
-// })
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { json } = require('sequelize');
 const router = express.Router();
 const apiRouter = require('./api');
 
