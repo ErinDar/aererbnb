479a6e9a368f54f4566f9d8d7358836aa503a5c4
Erin Dar <erindar@Erins-iMac.local>
2022-10-03 00:56:21 -0400

    Updated reviews/current route to test raw SQL

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -36,8 +36,8 @@ router.get('/current', requireAuth, async (req, res, next) => {
                         // Only works locally   
                         [
                             sequelize.literal(`(
-                                SELECT url FROM spotImages
-                                WHERE spotImages.spotId = spot.id
+                                'SELECT url FROM spotImages
+                                WHERE spotImages.spotId = spot.id'
                             )`), 'previewImage'
                         ]
                     ]
@@ -45,7 +45,8 @@ router.get('/current', requireAuth, async (req, res, next) => {
                 {
                     model: ReviewImage,
                     attributes: ['id', 'url']
-                }
+                },
+                SpotImage
             ]
         })
         return res.json({ Reviews })
